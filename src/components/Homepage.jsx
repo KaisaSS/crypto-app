78a7be3fed780b4f146d1680e756e78a941fa781
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-12-09 15:49:00 +0000

    Use Redux toolkit to fetch data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 
 const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+
+  console.log("data", data);
+
+  if (isFetching) return "Loading...";
+
   return (
     <>
       <Title level={2} className="heading">
