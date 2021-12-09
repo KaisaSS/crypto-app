5087786cc85f7d82c2075c26335112321f8db864
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-12-09 11:02:47 +0000

    Create main content components and add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from "./components";
 import "./App.css";
 
 const App = () => {
@@ -10,7 +10,19 @@ const App = () => {
         <div className="navbar">
           <Navbar />
         </div>
-        <div className="main"></div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Switch>
+                <Route exact path="/" component={Homepage} />
+                <Route path="/exchanges" component={Exchanges} />
+                <Route path="/cryptocurrencies" component={Cryptocurrencies} />
+                <Route path="/crypto/:coinId" component={CryptoDetails} />
+                <Route path="/crypto/news" component={News} />
+              </Switch>
+            </div>
+          </Layout>
+        </div>
         <div className="footer"></div>
       </div>
     </BrowserRouter>
