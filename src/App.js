5acb49fc8d039e331f98af6bd0da7bffd4e2e56f
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-12-09 10:16:36 +0000

    Create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>CryptoCurrs</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main"></div>
+        <div className="footer"></div>
+      </div>
+    </BrowserRouter>
   );
 };
 
