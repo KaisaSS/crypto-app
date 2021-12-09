2603406fab1616f469cc5191bb854cd2137645c0
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-12-09 10:38:15 +0000

    Add menu items and styling to navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 import { Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
