12d86ffff33723d1633f3313bb1012ab348a5b26
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-12-10 10:45:44 +0000

    Add searchbar to News

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, Typography, Row, Col, Card, Avatar } from "antd";
 import moment from "moment";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -9,26 +10,49 @@ const { Option } = Select;
 const demoImage = "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
 
 const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+
   const { data: cryptoNews } = useGetCryptoNewsQuery({
-    newsCategory: "Cryptocurrency",
+    newsCategory,
     count: simplified ? 6 : 12,
   });
+
+  const { data } = useGetCryptosQuery(100);
 
   if (!cryptoNews?.value) return "Loading...";
 
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {data?.data?.coins.map((currency) => (
+              <Option value={currency.name}>{currency.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews.value.map((news, i) => (
-        <Col xs={24} s={12} lg={8} key={i}>
+        <Col lg={12} xl={8} key={i}>
           <Card hoverable className="news-card">
             <a href={news.url} target="_blank" rel="noreferrer">
               <div className="news-image-container">
                 <Title className="news-title" level={4}>
-                  {news.name}
+                  {news.name.length > 100 ? `${news.name.substring(0, 100)}...` : news.name}
                 </Title>
                 <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
               </div>
-              <p>{news.description > 100 ? `${news.description.substring(0, 100)} ...` : news.description}</p>
+              <p>
+                {news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}
+              </p>
               <div className="provider-container">
                 <div>
                   <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="" />
