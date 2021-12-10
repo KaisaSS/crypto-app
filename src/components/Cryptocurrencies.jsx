9601c76e2102ca3efd16b358c16165045694d771
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-12-10 11:47:06 +0000

    Add content to CryptoDetails

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -29,7 +29,7 @@ const Cryptocurrencies = ({ simplified }) => {
       )}
       <Row gutter={[30, 30]} className="crypto-card-container">
         {cryptos?.map((currency) => (
-          <Col xs={24} s={12} lg={6} className="crypto-card" key={currency.id}>
+          <Col s={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
