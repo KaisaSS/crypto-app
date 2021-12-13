3520a6bca5d76f335bf413fbbf7b74df8c791e65
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-12-13 11:07:40 +0000

    Create Exchanges page

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -2,12 +2,71 @@ import React from "react";
 import millify from "millify";
 import { Collapse, Row, Col, Typography, Avatar } from "antd";
 import HTMLReactParser from "html-react-parser";
+import { useGetExchangesQuery } from "../services/cryptoApi";
+
+const { Text } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
+  const { data, isFetching } = useGetExchangesQuery();
+  console.log(data);
+  const exchangeList = data?.data?.exchanges;
+
+  if (isFetching) return "Loading...";
+
   return (
-    <div>
-      <h1>Exchanges</h1>
-    </div>
+    <>
+      <Row className="top-row">
+        <Col span={6} className="exchange-toprow exchange-item-first">
+          Exchanges
+        </Col>
+        <Col span={6} className="exchange-toprow">
+          24h Trade Volume
+        </Col>
+        <Col span={6} className="exchange-toprow">
+          Markets
+        </Col>
+        <Col span={6} className="exchange-toprow">
+          Change
+        </Col>
+      </Row>
+      <Row className="exchange-row">
+        {exchangeList.map((exchange) => (
+          <Col span={24}>
+            <Collapse>
+              <Panel
+                key={exchange.id}
+                showArrow={false}
+                header={
+                  <Row key={exchange.id} className="exchange-row">
+                    <Col span={6} className="exchange-item-first">
+                      <Text>
+                        <strong>{exchange.rank}.</strong>
+                      </Text>
+                      <Avatar className="exchange-image" src={exchange.iconUrl} />
+                      <Text>
+                        <strong>{exchange.name}</strong>
+                      </Text>
+                    </Col>
+                    <Col span={6} className="exchange-item">
+                      ${millify(exchange.volume)}
+                    </Col>
+                    <Col span={6} className="exchange-item">
+                      {millify(exchange.numberOfMarkets)}
+                    </Col>
+                    <Col span={6} className="exchange-item">
+                      {millify(exchange.marketShare)}%
+                    </Col>
+                  </Row>
+                }
+              >
+                {HTMLReactParser(exchange.description || "")}
+              </Panel>
+            </Collapse>
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -20,7 +20,11 @@ export const cryptoApi = createApi({
     getCryptoHistory: builder.query({
       query: ({ coinId, timePeriod }) => createRequest(`/coin/${coinId}/history/${timePeriod}`),
     }),
+    getExchanges: builder.query({
+      query: () => createRequest(`/exchanges`),
+    }),
   }),
 });
 
-export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery } =
+  cryptoApi;
